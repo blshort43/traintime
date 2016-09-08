3e322b9e358224f4afb390cd7dd7632463903f81
Brandon <blshort43@gmail.com>
2016-09-07 19:40:10 -0500

    javascript, next train time working in console

---
+++ assets/javascript/train.js
@@ -0,0 +1,69 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAIn5XZsjrPd5jI55o_q3QzglwaRuwbYm8",
+    authDomain: "traintime-c84af.firebaseapp.com",
+    databaseURL: "https://traintime-c84af.firebaseio.com",
+    storageBucket: "traintime-c84af.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  var trainName, trainDestination, firstTrain, trainFrequency;
+
+  // Button to add trains
+  $("#addTrainButton").on("click", function(){
+
+  	// Get user input
+  	trainName = $("#trainInput").val().trim();
+  	trainDestination = $("#destinationInput").val().trim();
+  	firstTrain = $("#firstTrainInput").val().trim();
+  	trainFrequency = $("#frequencyInput").val().trim();
+
+  	// Local temporary info object
+  	var newTrain = {
+  		name: trainName,
+  		destination: trainDestination,
+  		first: firstTrain,
+  		frequency: trainFrequency
+  	};
+
+  	// upload data to database
+  	database.ref().push(newTrain);
+
+  	// console log
+  	console.log(newTrain.name);
+  	console.log(newTrain.destination);
+  	console.log(newTrain.first);
+  	console.log(newTrain.frequency);
+
+  	// clear text boxes
+  	$("#trainInput").val("");
+  	$("#destinationInput").val("");
+  	$("#firstTrainInput").val("");
+  	$("#frequencyInput").val("");
+
+  });
+
+  	// Firebase event for adding train to database & row in html when user adds input
+  	database.ref().on("child_added", function(childSnapshot){
+
+  	console.log(childSnapshot.val());
+
+  	// Store in variable
+ 	trainName = childSnapshot.val().name;
+ 	trainDestination = childSnapshot.val().destination;
+ 	firstTrain = childSnapshot.val().first;
+ 	trainFrequency = childSnapshot.val().frequency;
+
+ 	// Train info
+ 	console.log(trainName);
+ 	console.log(trainDestination);
+ 	console.log(firstTrain);
+ 	console.log(trainFrequency);
+
+ 	//calculate the time of the next train's arrival
+ 	var nextTrain = moment(firstTrain,"hh:mm").add(trainFrequency, "minutes");
+ 	console.log(moment(nextTrain, "hh:mm").format("LTS"));
+  })
